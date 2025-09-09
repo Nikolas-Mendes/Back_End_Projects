44e24dcbf1291161d543196dd86eccb203ca29d3
Nikolas Mendes <63744725+Nikolas-Mendes@users.noreply.github.com>
2025-09-09 15:22:32 -0300

    Add files via upload

---
+++ busca_cep/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+	Scanner leitura = new Scanner(System.in);
+	ConsultaCep consultaCep = new ConsultaCep();
+
+	System.out.println("Digite um número de CEP para consulta:");
+	var cep = leitura.nextLine();
+
+	try {
+	    Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+	    System.out.println(novoEndereco);
+	    GeradorDeArquivo gerador = new GeradorDeArquivo();
+	    gerador.salvaJson(novoEndereco);
+	} catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+	}	
+    }
+}
--- screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,4 +1,7 @@
 package br.com.alura.screenmatch.modelos;
+import com.google.gson.annotations.SerializedName;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import br.com.alura.screenmatch.exception.ErroDeConversaoDeAnoException;
 
 public class Titulo implements Comparable<Titulo> {
     private String nome;
@@ -11,6 +14,17 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
 	this.nome = nome;
 	this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+	this.nome = meuTituloOmdb.title();
+
+	if(meuTituloOmdb.year().length() > 4) {
+	    throw new ErroDeConversaoDeAnoException("Não Consegui converter o ano porque tem mais de 04 caracteres.");
+	}
+
+	this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+	this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2);
     }
 
     public String getNome() {
@@ -66,5 +80,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
 	return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+	return "Titulo: " + nome + 
+	       "\nAno de Lançamento: " + anoDeLancamento + 
+	       "\nDuração: " + duracaoEmMinutos;
     }
 }
